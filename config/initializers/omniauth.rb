9d5899294387bfaeb28cca4a06ba5d383afe0bdd
Bryce Holcomb <bryce_holcomb@outlook.com>
2015-03-02 20:26:21 -0700

    add full host to omniauth for production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :uber, ENV['UBER_CLIENT_ID'], ENV['UBER_SECRET']
 end
+
+if Rails.env.production?
+  OmniAuth.config.full_host = "https://rider-demand.herokuapp.com"
+end
